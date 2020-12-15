a3f7dc519b02bb0f1a68ec95b392f94914e6db7e
Dmitriy Khristoev <haossut@gmail.com>
2020-12-15 23:19:38 +0200

    промежуточный коммит

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -38,8 +38,13 @@ $(document).on('ready', function(){
     $(this).prepend('<span class="span">' + count + '</span>');
   });
 
+  $("form").each(function(){
+    $(this).validate();
+  })
+
   mobileNav();
   footerNav();
+  phoneMask();
 
   // Chrome Smooth Scroll
   try {
@@ -118,3 +123,31 @@ function footerNav() {
     });
   })
 }
+
+function phoneMask() {
+  var phone = $('.j-phone-mask');
+  phone.each(function () {
+    $(this).mask("+7 (999) 999-99-99");
+  })
+}
+
+jQuery.extend(jQuery.validator.messages, {
+  required: "Обязательное поле",
+  remote: "Please fix this field.",
+  email: "Введите правильный e-mail.",
+  url: "Please enter a valid URL.",
+  date: "Please enter a valid date.",
+  dateISO: "Please enter a valid date (ISO).",
+  number: "Please enter a valid number.",
+  digits: "Please enter only digits.",
+  creditcard: "Please enter a valid credit card number.",
+  equalTo: "Пароли не совпадают.",
+
+  accept: "Please enter a value with a valid extension.",
+  maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+  minlength: jQuery.validator.format("Please enter at least {0} characters."),
+  rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+  range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+  max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+  min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
