bd30234bd46804a3f71bdb72e31b1a2cfa33ab31
Dmitriy Khristoev <haossut@gmail.com>
2020-12-30 17:47:15 +0200

    fix

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -79,6 +79,9 @@ $(document).on('ready', function(){
   } catch(err) {
 
   };
+
+  simpleForm('form#form_1');
+  simpleForm('form#form_2');
 });
 
 $(window).on('load', function() { });
@@ -197,4 +200,68 @@ function cartCountTest() {
       input.val(value);
     });
   })
+}
+
+/*
+version 2015-09-23 14:30 GMT +2
+*/
+function simpleForm(form, callback) {
+  $(document).on('submit', form, function(e){
+    e.preventDefault();
+    var formData = {};
+    var hasFile = false;
+    if ($(this).find('[type=file]').length < 1) {
+      formData = $(this).serialize();
+    }
+    else {
+      formData = new FormData();
+      $(this).find('[name]').each(function(){
+
+        switch($(this).prop('type')) {
+
+          case 'file':
+            if ($(this)[0]['files'].length > 0) {
+              formData.append($(this).prop('name'), $(this)[0]['files'][0]);
+              hasFile = true;
+            }
+            break;
+
+          case 'radio':
+          case 'checkbox':
+            if (!$(this).prop('checked')) {
+              break;
+            }
+            formData.append($(this).prop('name'), $(this).val().toString());
+            break;
+
+          default:
+            formData.append($(this).prop('name'), $(this).val().toString());
+            break;
+        }
+      });
+    }
+
+    $.ajax({
+      url: $(this).prop('action'),
+      data: formData,
+      type: 'POST',
+      contentType : hasFile ? 'multipart/form-data' : 'application/x-www-form-urlencoded',
+      cache       : false,
+      processData : false,
+      success: function(response) {
+        $(form).removeClass('ajax-waiting');
+        $(form).find("[type=submit]").prop("disabled", false);
+        $(form).html($(response).find(form).html());
+
+        if (typeof callback === 'function') {
+          callback(response);
+        }
+      }
+    });
+
+    $(form).addClass('ajax-waiting');
+    $(form).find("[type=submit]").prop("disabled", true);
+
+    return false;
+  });
 }
